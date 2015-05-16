ff9b792e0dcefce2a0ed6c67626c015cf3ed5ea6
natalia <nataliad2412@gmail.com>
2015-05-16 02:05:11 -0700

    animal collection ViewModel added

---
+++ OstatnieSuchary/OstatnieSuchary/ViewModel/AnimalsCollectionViewModel.cs
@@ -0,0 +1,24 @@
+﻿using OstatnieSuchary.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OstatnieSuchary.ViewModel
+{
+    class AnimalsCollectionViewModel : Model.ViewModel
+    {
+        public ObservableCollection<Animal> AllAnimalsCollection { get; set; }
+
+        public AnimalsCollectionViewModel()
+        {
+            AllAnimalsCollection = new ObservableCollection<Animal>();
+            //AllAnimalsCollection.Add(new Lemoor(:D));
+            //AllAnimalsCollection.Add(new Lemoor(:D));
+            //AllAnimalsCollection.Add(new Lemoor(:D));
+        }
+
+    }
+}
