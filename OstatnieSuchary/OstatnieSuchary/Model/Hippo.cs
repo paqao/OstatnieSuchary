e595ea7050d8e46dc96e66a775b3edee0080c1bb
natalia <nataliad2412@gmail.com>
2015-05-16 02:14:54 -0700

    Animals models added

---
+++ OstatnieSuchary/OstatnieSuchary/Model/Hippo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OstatnieSuchary.Model
+{
+    class Hippo : Animal
+    {
+        public Hippo(string name) : base(name, AnimalType.Hippo)
+		{
+        }
+    }
+}
---
+++ OstatnieSuchary/OstatnieSuchary/Model/Pinguins.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OstatnieSuchary.Model
+{
+    class Pinguins : Animal
+    {
+        public Pinguins(string name) : base(name, AnimalType.Pinguins)
+		{
+        }
+    }
+}
